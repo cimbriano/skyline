53a588711052f1568d238f0974a518ed9c20361a
cimbriano <Christopher.Imbriano@gmail.com>
2014-04-26 14:10:59 -0400

    Pipeline makes sure a directory exists in out/ for this pipeline

--- pipeline.rb
+++ pipeline.rb
@@ -1,12 +1,22 @@
+require 'fileutils'
+
 class Pipeline
+
+
   def initialize(f)
     @filename = f
   end
 
   def run(opts)
+    setup
     midi  if opts[:midi]  || opts[:all]
     stats if opts[:stats] || opts[:all]
     scad  if opts[:scad]  || opts[:all]
+  end
+
+  def setup
+    # Make sure out directory exists
+    FileUtils::mkdir_p "./out/#{basename}"
   end
 
   def midi
@@ -19,5 +29,10 @@ class Pipeline
 
   def scad
     puts "Doing scad stage for #{@filename}"
+  end
+
+  # Removes the extension from the filename
+  def basename
+    @basename ||= File.basename(@filename, '.*')
   end
 end

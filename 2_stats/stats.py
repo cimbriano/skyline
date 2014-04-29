2b5f14c9e0c6824aef061ea64fbc56729c7c1fd7
Max Potasznik <maxp888@gmail.com>
2014-04-29 00:39:42 -0400

    added stats.py. pipeline stage:2. processes notes_file.csv into stats_file.json

---
+++ 2_stats/stats.py
@@ -0,0 +1,50 @@
+'''
+Created on Apr 29, 2014
+
+@author: maxp
+'''
+
+
+import argparse
+import pandas as pd
+import numpy as np
+
+
+def freneticity(col):
+    return np.diff(col).mean()
+
+
+def main():
+    parser = argparse.ArgumentParser(prog='stats')
+    parser.add_argument('path_name',
+                        help='path to the pipeline files')
+    parser.add_argument('--mode',default=0, choices=[0,1],
+                        help='aggregation mode')
+    args = parser.parse_args()
+    
+    
+    
+    note_file = args.path_name + 'notes_file_' + args.path_name[:-1] + '.csv'
+    stats_file = args.path_name + 'stats_file_' + args.path_name[:-1] + '.json'
+    
+    
+    note_df = pd.read_csv(note_file)
+    agg_df = note_df.groupby(['letter','octave'])
+    cnt = agg_df.letter.count()
+    avgLen = agg_df.duration.mean()
+    avgVel = agg_df.velocity.mean()
+    fren = agg_df.time_on.apply(freneticity)
+    fren.fillna(0, inplace=True)
+
+    
+    cnt.name = 'count'
+    avgLen.name = 'avgLen'
+    avgVel.name = 'avgVelocity'
+    fren.name = 'freneticity'
+    
+    ans  = pd.concat([cnt, avgVel, avgLen, fren], axis=1)
+    ans.to_json(stats_file,orient='index')
+
+
+if __name__ == '__main__':
+    main()
